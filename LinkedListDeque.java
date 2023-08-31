92ea1393f833cd17490cdbc8d8050ab57c5104a8
musictalentphongcach <63213514+musictalentphongcach@users.noreply.github.com>
2023-08-30 23:50:15 -0700

    Add files via upload

--- LinkedListDeque.java
+++ LinkedListDeque.java
@@ -1,100 +1,122 @@
-public class LinkedListDeque<Gen> {
-    public IntNode first;
-    public int size;
+public class LinkedListDeque<T> {
+    private class Node {
+        private Node prev;
+        private T item;
+        private Node next;
 
-    public LinkedListDeque() {
-        first = null;
+        public Node(LinkedListDeque<T>.Node prev, T item, LinkedListDeque<T>.Node next) {
+            this.prev = prev;
+            this.item = item;
+            this.next = next;
+        }
+    }
+
+    private Node sentinel;
+    private int size;
+
+    public LinkedListDeque() { //emty LL
+        sentinel = new Node(null, (T) new Object(), null);
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
         size = 0;
     }
 
-    public int size(){
+    /** Adds an item of type T to the front of the deque. */
+    public void addFirst(T item) { // add first function
+        Node newNode = new Node(sentinel, item, sentinel.next);
+        sentinel.next.prev = newNode;
+        sentinel.next = newNode;
+        size++;
+    }
+
+
+    public void addLast(T item) { // add an item to last of list
+        Node newNode = new Node(sentinel.prev, item, sentinel);
+        sentinel.prev.next = newNode;
+        sentinel.prev = newNode;
+        size++;
+    }
+
+
+    public boolean isEmpty() { // true false for empty
+        return size == 0;
+    }
+
+
+    public int getSize() { //return the size of the dq
         return size;
     }
-    public LinkedListDeque(Gen x) {
-        first = new IntNode(x, null);
-        first.next = first;
-        size++;
-    }
 
-    public void addFirst(Gen x) {
-        size++;
-        first = new IntNode(x, first);
-    }
-
-    public void addLast(Gen x) {
-        size++;
-        IntNode p = first;
-        while (p.next != null) {
-            p = p.next;
+    public void printDeque() { // print out the LL
+        // sentinel.next is the next item
+        for (Node i = sentinel.next; i != sentinel; i = i.next) {
+            if (i.next == sentinel) {
+                System.out.println(i.item);
+                break;
+            }
+            System.out.print(i.item + " ");
         }
-        first = new IntNode(x, first);
     }
 
-    public int sizeList() {
-        return size;
-    }
-
-    public void printDeque() {
-        IntNode p = first;
-        while (p != null) {
-            System.out.print(p.item + " ");
-            p = p.next;
-        }
-        System.out.println();
-    }
-
-    public Gen removeFirst() {
-        if (first == null) {
+    /**
+     * Removes and returns the item at the front of the deque. If no such item
+     * exists, returns null.
+     */
+    public T removeFirst() {
+        if (isEmpty()) {
             return null;
         }
-        Gen removedItem = first.item;
-        first = null;
+        T res = sentinel.next.item;
+        sentinel.next = sentinel.next.next;
+        sentinel.next.prev = sentinel;
         size--;
-        return removedItem;
+        return res;
     }
 
-    public Gen removeLast() { //remove last item of the LL
-        if (first == null) { //check if the list exist
+    /**
+     * Removes and returns the item at the back of the deque. If no such item
+     * exists, returns null.
+     */
+    public T removeLast() {
+        if (isEmpty()) {
             return null;
         }
-        if (first.next == null) { // if the list only has 2 item remove the item and decrease by 1
-            Gen removedItem = first.item;
-            first = null;
-            size--;
-            return removedItem;
-        }
-        IntNode p = first; // use the pointer p to traverse thru the LL
-        while (p.next.next != null) { // check if the second to last node is valid
-            p = p.next;
-        }
-        Gen removedItem = p.next.item; // initialize Gen object to store the remove item
-        p.next = null;
+        T res = sentinel.prev.item;
+        sentinel.prev = sentinel.prev.prev;
+        sentinel.prev.next = sentinel;
         size--;
-        return removedItem;
+        return res;
     }
 
-    public Gen get(int x) { // get the item at the specified index
-        if (x < 0 || x >= size) { // check the range of the index if invalid return null
+    /**
+     * Gets the item at the given index, where 0 is the front, 1 is the next item,
+     * and so forth. If no such item exists, returns null. Must not alter the deque!
+     */
+    public T get(int index) {
+        if (size < index) {
             return null;
         }
-        IntNode p = first; // use pointer p to traverse thru the array
-        for (int i = 0; i < x; i++) {
+        Node p = sentinel.next;
+        while (index > 0) {
             p = p.next;
+            index--;
         }
         return p.item;
     }
 
-    public boolean isEmpty() {
-        return first == null;
-    }
-
-    public class IntNode {
-        public Gen item;
-        IntNode next;
-
-        public IntNode(Gen i, IntNode n) {
-            this.item = i;
-            this.next = n;
+    /** Same as get, but uses recursion. */
+    public T getRecursive(int index) {
+        if (size < index) {
+            return null;
         }
+        return getRecursive(sentinel.next, index);
     }
+
+    private T getRecursive(LinkedListDeque<T>.Node node, int i) {
+        if (i == 0) {
+            return node.item;
+        }
+        return getRecursive(node.next, i - 1);
+    }
+
 }
