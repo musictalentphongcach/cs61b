1eb5dae7395de2d8cb742853f9e454eadcd75dd3
musictalentphongcach <63213514+musictalentphongcach@users.noreply.github.com>
2023-09-06 03:52:29 -0700

    Update ArrayDeque.java

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,152 +1,112 @@
 public class ArrayDeque<T> {
     private T[] items; // Array to store deque items
-    private int nextFirst; // Index for the next first item
-    private int nextLast; // Index for the next last item
-    private int size; // Current number of items in the deque
-
-    private static final int startSize = 8; // Initial capacity of the array
-    private static final int reFact = 2; // Resizing factor for the array
+    private int left; // Index for the front of the deque
+    private int right; // Index for the back of the deque
+    private int capacity = 8; // Initial capacity of the array
 
     public ArrayDeque() {
-        items = (T[]) new Object[startSize]; // Initialize array with initial capacity
-        nextFirst = 0; // Index for the first item (empty)
-        nextLast = 1; // Index for the second item (empty)
-        size = 0; // Initialize size to zero (empty deque)
+        items = (T[]) new Object[capacity]; // Initialize array with initial capacity
+        left = 0; // Index for the front of the deque
+        right = 0; // Index for the back of the deque
     }
 
-    // Calculate the index for the previous position in the array (circular)
-    private void getFront() {
-
-        nextFirst = minusOne(nextFirst);
-    }
-
-    // Calculate the index for the next position in the array (circular)
-    private void getLast() {
-        nextLast = plusOne(nextLast);
-    }
-
-    // Calculate the next index in circular fashion
-    private int plusOne(int index) {
-        return (index + 1) % items.length;
-    }
-
-    // Calculate the previous index in circular fashion
-    private int minusOne(int index) {
-
-        return (index + items.length - 1) % items.length;
-    }
-
-    // Check if the deque is full
-    private boolean isFull() {
-        return size == items.length;
-    }
-
-    // Resize the array to a new capacity
-    private void resize(int newCapacity) {
-        T[] newArray = (T[]) new Object[newCapacity]; // Create a new array with the new capacity
-        int start = plusOne(nextFirst); // Find the index of the first item in the new array
-        for (int i = 0; i < size; i++) {
-            newArray[i] = items[start]; // Copy items from the old array to the new array
-            start = plusOne(start); // Move to the next item in the old array
-        }
-        items = newArray; // Update the array reference
-        nextFirst = newArray.length - 1; // Update the index for the first item
-        nextLast = size; // Update the index for the next item
-    }
-
-    // Check if resizing is needed based on usage
-    private void checkSize() {
-        if (isSparse()) {
-            reSmall(); // Shrink the array if usage is low
-        } else if (isFull()) {
-            reLarge(); // Extend the array if it is full
-        }
-    }
-
-    // Add an item to the front of the deque
+    // Adds an item to the front of the deque
     public void addFirst(T item) {
-        items[nextFirst] = item; // Add the item to the nextFirst position
-        getFront(); // Update nextFirst index
-        size++; // Increase the size
-        checkSize(); // Check if resizing is needed
+        if (isFull()) { // Check if the array is full
+            resize((int) (capacity * 2)); // If full, resize the array to increase capacity
+        }
+        left = (left - 1 + capacity) % capacity; // Calculate the new front index using circula method
+        items[left] = item; // Add the item at the new front index
     }
 
-    // Add an item to the end of the deque
+    // Adds an item to the back of the deque
     public void addLast(T item) {
-        items[nextLast] = item; // Add the item to the nextLast position
-        getLast(); // Update nextLast index
-        size++; // Increase the size
-        checkSize(); // Check if resizing is needed
-
+        if (isFull()) { // Check if the deque is full
+            resize((int) (capacity * 2)); // If full, resize the array to increase capacity
+        }
+        items[right] = item; // Add the item at the current back index
+        right = (right + 1) % capacity; // Calculate the new back index in a circular method
     }
 
+    // Returns true if deque is empty, false otherwise
+    public boolean isEmpty() {
+        return left == right; // If the front and back indices are the same, the deque is empty
+    }
 
-
-    // Get the current size of the deque
+    // Returns the number of items in the deque
     public int size() {
-        return size;
+        return (right - left + capacity) % capacity; // Calculate the size of the deque in a circular manner
     }
 
-    // Check if the array is sparsely used
-    private boolean isSparse() {
-        return items.length >= 16 && size < items.length / 4;
-    }
-
-    // Resize the array to a smaller capacity
-    private void reSmall() {
-        resize(items.length / reFact); // divide the reFactor to get the smaller array size
-    }
-
-    // Resize the array to a larger capacity
-    private void reLarge() {
-        resize(reFact * items.length); // multiply the Array size by 2
-    }
-
-    // Remove the first item from the deque
-    public T removeFirst() {
-        if(isEmpty()){
-            return null;
-        }
-        T returnItem = items[plusOne(nextFirst)]; // Get the item to be removed
-        items[plusOne(nextFirst)] = null; // Clear the item
-        nextFirst = plusOne(nextFirst); // Update nextFirst index
-
-        size--; // Decrease the size
-        checkSize(); // see if resizing is rneeded
-        return returnItem; // Return the removed item
-    }
-
-    // Remove the last item from the deque
-    public T removeLast() {
-        if(isEmpty()){
-            return null;
-        }
-        T returnItem = items[minusOne(nextLast)]; // Get the item to be removed
-        items[minusOne(nextLast)] = null; // Clear the item
-        nextLast = minusOne(nextLast); // Update nextLast index
-        size--; // Decrease the size
-        checkSize(); // Check if resizing is needed
-        return returnItem; // Return the removed item
-    }
-
-    //check if the array is empty set the head = tail.
-    public boolean isEmpty(){
-        return nextFirst == nextLast;
-    }
-
-    // Print the contents of the deque
+    // Prints the items in the deque from first to last, separated by a space
     public void printDeque() {
-        for (int i = plusOne(nextFirst), count = 0; count < size; i = plusOne(i), count++) {
-            System.out.print(items[i] + " "); // Print each item in the deque
+        int size = size(); // Get the size of the deque
+        for (int i = 0; i < size; i++) {
+            int index = (left + i) % capacity; // Calculate the index for the current item
+            if (i == size - 1) {
+                System.out.println(items[index]); // Print the last item with a newline
+            } else {
+                System.out.print(items[index] + " "); // Print the item with a space separator
+            }
         }
-        System.out.println(); // Print a new line at the end
     }
 
-    // Get an item at the specified index
-    public T get(int index) {
-        if (index < 0 || index >= size) {
-            return null; // Return null if index is out of bounds
+    // Removes and returns the item at the front of the deque
+    public T removeFirst() {
+        if (isEmpty()) { // Check if the deque is empty
+            return null; // If empty, return null
         }
-        return items[(plusOne(nextFirst) + index) % items.length]; // Get the item using circular indexing
+        T res = items[left]; // Get the item at the front
+        left = (left + 1) % capacity; // Calculate the new front index in a circular manner
+        if (isLowUsageRate()) { // Check if resizing is needed based on usage
+            resize((int) (capacity * 0.5)); // Resize the array to reduce capacity
+        }
+        return res; // Return the removed item
+    }
+
+    // Removes and returns the item at the back of the deque
+    public T removeLast() {
+        if (isEmpty()) { // Check if the deque is empty
+            return null; // If empty, return null
+        }
+        right = (right - 1 + capacity) % capacity; // Calculate the new back index in a circular manner
+        T res = items[right]; // Get the item at the back
+        if (isLowUsageRate()) { // Check if resizing is needed based on usage
+            resize((int) (capacity * 0.5)); // Resize the array to reduce capacity
+        }
+        return res; // Return the removed item
+    }
+
+    // Gets the item at the given index
+    public T get(int index) {
+        if (index < 0 || index >= size() || isEmpty()) { // Check for invalid index or empty deque
+            return null; // If invalid, return null
+        }
+        int itemIndex = (left + index) % capacity; // Calculate the index for the requested item
+        return items[itemIndex]; // Return the item at the calculated index
+    }
+
+    // Checks if the deque is full
+    private boolean isFull() {
+        return size() == capacity - 1; // Check if the size is equal to one less than capacity
+    }
+
+    // Checks if resizing is needed based on usage
+    private boolean isLowUsageRate() {
+        return capacity >= 16 && size() / (double) capacity < 0.25; // Check if the usage rate is low
+    }
+
+    // Resizes the array to a new capacity
+    private void resize(int newSize) {
+        T[] newArray = (T[]) new Object[newSize]; // Create a new array with the specified capacity
+        int size = size(); // Get the current size of the deque
+        for (int i = 0; i < size; i++) { // traverse thru the array
+            int index = (left + i) % capacity; // Calculate the index for the current item
+            newArray[i] = items[index]; // Copy items to the new array
+        }
+        left = 0; // Reset the front index
+        right = size; // Set the back index to the new size
+        items = newArray; // Update the deque to use the new array
+        capacity = newSize; // Update the capacity
     }
 }
