2bc79aaf2f8d44de9812b8d46302be9be7ab67af
musictalentphongcach <63213514+musictalentphongcach@users.noreply.github.com>
2023-09-07 01:29:09 -0700

    Update LinkedListDeque.java

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -45,7 +45,7 @@ public class LinkedListDeque<T> {
     }
 
 
-    public int getSize() { //return the size of the dq
+    public int size() { //return the size of the dq
 
         return size;
     }
