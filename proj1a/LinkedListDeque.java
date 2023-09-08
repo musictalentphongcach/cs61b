4ac0f12920714870b892d248f480c0dc534f9c10
musictalentphongcach <63213514+musictalentphongcach@users.noreply.github.com>
2023-09-08 03:36:03 -0700

    Update LinkedListDeque.java

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,75 +1,82 @@
+/**
+ * Deque (usually pronounced like “deck”) is an irregular acronym of
+ * double-ended queue. Double-ended queues are sequence containers with dynamic
+ * sizes that can be expanded or contracted on both ends (either its front or
+ * its back).
+ */
 public class LinkedListDeque<T> {
-    private Node sentinel; // sentinel node for dummy
-    private int size; // size counter
-    private class Node { // invariants
+    private class Node {
+        private Node prev;
+        private T item;
+        private Node next;
 
-        private Node prev; // dummy node for last
-        private T item; // initialize Generic type T item
-        private Node next; // dummy node for first
-        // create one last bummy node and one first dummy node referencing the prev and next
         public Node(LinkedListDeque<T>.Node prev, T item, LinkedListDeque<T>.Node next) {
-            this.prev = prev; // store the last reference node
-            this.item = item; //store the element of the node
-            this.next = next; // store the first reference node
+            this.prev = prev;
+            this.item = item;
+            this.next = next;
         }
     }
 
+    private Node sentinel;
+    private int size;
 
-
-    public LinkedListDeque() { //emty LL
+    public LinkedListDeque() {
         sentinel = new Node(null, (T) new Object(), null);
-        sentinel.prev = sentinel;// set the first point to the sent
-        sentinel.next = sentinel; // set the last point to the sent
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
         size = 0;
     }
 
     /** Adds an item of type T to the front of the deque. */
-    public void addFirst(T item) { // add first function
+    public void addFirst(T item) {
         Node newNode = new Node(sentinel, item, sentinel.next);
         sentinel.next.prev = newNode;
         sentinel.next = newNode;
         size++;
     }
 
-
-    public void addLast(T item) { // add an item to last of list
+    /** Adds an item of type T to the back of the deque. */
+    public void addLast(T item) {
         Node newNode = new Node(sentinel.prev, item, sentinel);
         sentinel.prev.next = newNode;
         sentinel.prev = newNode;
         size++;
     }
 
-
-    public boolean isEmpty() { // true false for empty
+    /** Returns true if deque is empty, false otherwise. */
+    public boolean isEmpty() {
         return size == 0;
     }
 
-
-    public int size() { //return the size of the dq
-
+    /** Returns the number of items in the deque. */
+    public int size() {
         return size;
     }
 
-    public void printDeque() { // print out the LL
-        // sentinel.next is the next item
+    /** Prints the items in the deque from first to last, separated by a space. */
+    public void printDeque() {
         for (Node i = sentinel.next; i != sentinel; i = i.next) {
-            if (i.next == sentinel) { // if the next item of the index is the sentinel which is the head
-                System.out.println(i.item);// print out the last item that the index was on
-                break; // code will stop
+            if (i.next == sentinel) {
+                System.out.println(i.item);
+                break;
             }
             System.out.print(i.item + " ");
         }
     }
 
-    public T removeFirst() { // if the list is empty return null
+    /**
+     * Removes and returns the item at the front of the deque. If no such item
+     * exists, returns null.
+     */
+    public T removeFirst() {
         if (isEmpty()) {
             return null;
         }
-        T remove = sentinel.next.item; // traverse thru the dummy node first
-        sentinel.next = sentinel.next.next; // set the node pointing to the node after removed node
-        sentinel.next.prev = sentinel; // set the sent node to be the head
+        T res = sentinel.next.item;
+        sentinel.next = sentinel.next.next;
+        sentinel.next.prev = sentinel;
         size--;
-        return remove;
+        return res;
     }
 
     /**
@@ -80,11 +87,11 @@ public class LinkedListDeque<T> {
         if (isEmpty()) {
             return null;
         }
-        T remove = sentinel.prev.item;
+        T res = sentinel.prev.item;
         sentinel.prev = sentinel.prev.prev;
         sentinel.prev.next = sentinel;
         size--;
-        return remove;
+        return res;
     }
 
     /**
@@ -111,7 +118,7 @@ public class LinkedListDeque<T> {
         return getRecursive(sentinel.next, index);
     }
 
-    private T getRecursive(LinkedListDeque<T>.Node node, int i) { // helper recursive
+    private T getRecursive(LinkedListDeque<T>.Node node, int i) {
         if (i == 0) {
             return node.item;
         }
